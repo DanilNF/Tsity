c0dfae83bac0ea4de6622fba395b880e0b01f6a1
TimCorse <dopystimmail@mail.ru>
2021-06-05 06:45:05 +0500

    добавление товара в main + страница товара

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 const express       = require('express');
 const MongoClient   = require('mongodb').MongoClient;
 const bodyParser    = require('body-parser');
@@ -9,9 +8,12 @@ const jsonParser = express.json();
 
 const mongoClient = new MongoClient("mongodb://localhost:27017/", { useUnifiedTopology: true });
 const urlencodedParser = bodyParser.urlencoded({extended: false});
- 
-let dbClient;
 
+
+let dbClient;
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.set("view engine", "hbs");
 app.engine("hbs",hbs(
     {
@@ -24,20 +26,17 @@ app.engine("hbs",hbs(
 app.use(express.static(__dirname + "/src"));
  
 mongoClient.connect(function(err, client){
-    if(err) return //console.log(err);
+    if(err) return console.log(err);
     dbClient = client;
     app.locals.collection = client.db("usersdb").collection("users");
-	app.locals.collection.find({}).toArray(function(err, users){
-        console.log(users);
-    });
+	//app.locals.collection.find({}).toArray(function(err, users){
+        //console.log(users);
+    //});
     app.listen(9000, function(){
         console.log("Сервер ожидает подключения на 9000...");
     });
 });
 
-// app.get("/auth", urlencodedParser, function (request, response) {
-//     response.sendFile(__dirname+"/src/auth.html");
-// });
 
 app.post("/auth", urlencodedParser, function (request, response) {
     if(!request.body) return response.sendStatus(400);
@@ -71,8 +70,8 @@ app.post("/register", urlencodedParser, function (request, response) {
     const user = {name: userName, password: userPassword, email: userEmail , number:userNumber};
     const collection = request.app.locals.collection;
     collection.insertOne(user, function(err, result){
-        if(err) return// console.log(err);
-     //   response.send(user);
+        if(err) return //console.log(err);
+      response.send(user);
 		response.render(__dirname+"/src/lk-hbs.hbs",{
 			name: user.name,
 			email: user.email,
@@ -82,7 +81,52 @@ app.post("/register", urlencodedParser, function (request, response) {
     });
 });
 app.get("/", function(request, response){
-    response.render(__dirname+"/src/main.hbs", {
+    const db = mongoClient.db("tovarsDB");
+	const collection = db.collection("tovars");
+	collection.find({}).toArray(function(err, tovars){
+          response.render(__dirname+"/src/main.hbs", {
+            image1: tovars[0].photo,
+            text1: tovars[0].text,
+            price1: tovars[0].price,
+			image2: tovars[1].photo,
+             text2: tovars[1].text,
+            price2: tovars[1].price,
+			image3: tovars[2].photo,
+             text3: tovars[2].text,
+            price3: tovars[2].price,
+			image4: tovars[3].photo,
+             text4: tovars[3].text,
+            price4: tovars[3].price,
+			image5: tovars[4].photo,
+             text5: tovars[4].text,
+            price5: tovars[4].price,
+			image6: tovars[5].photo,
+             text6: tovars[5].text,
+            price6: tovars[5].price,
+			image7: tovars[6].photo,
+             text7: tovars[6].text,
+            price7: tovars[6].price,
+			image8: tovars[7].photo,
+             text8: tovars[7].text,
+            price8: tovars[7].price,
+			image9: tovars[8].photo,
+             text9: tovars[8].text,
+            price9: tovars[8].price,
+			image10: tovars[9].photo,
+             text10: tovars[9].text,
+            price10: tovars[9].price,
+			image11: tovars[10].photo,
+             text11: tovars[10].text,
+            price11: tovars[10].price,
+			image12: tovars[11].photo,
+             text12: tovars[11].text,
+            price12: tovars[11].price,
+
+          })
+    });	
+ });
+ app.get("/index-tovar", function(request, response){
+    response.render(__dirname+"/src/index-tovar.hbs", {
     });
  });
 
@@ -119,22 +163,24 @@ app.get("/reg", function(request, response){
         password: ""
     });    
 });
+
 app.get("/api/users", function(req, res){
         
     const collection = req.app.locals.collection;
     collection.find({}).toArray(function(err, users){
          
-        if(err) return //console.log(err);
+        if(err) return console.log(err);
         res.send(users)
     });
      
 });
+
 app.get("/api/users/:id", function(req, res){
         
     const id = new objectId(req.params.id);
     const collection = req.app.locals.collection;
     collection.findOne({_id: id}, function(err, user){
-        if(err) return// console.log(err);
+        if(err) return console.log(err);
         res.send(user);
     });
 });
@@ -151,7 +197,7 @@ app.post("/api/users", jsonParser, function (req, res) {
     const collection = req.app.locals.collection;
     collection.insertOne(user, function(err, result){
                
-        if(err) return// console.log(err);
+        if(err) return console.log(err);
         res.send(user);
     });
 });
@@ -161,7 +207,7 @@ app.delete("/api/users/:id", function(req, res){
     const collection = req.app.locals.collection;
     collection.findOneAndDelete({_id: id}, function(err, result){
 
-        if(err) return //console.log(err);    
+        if(err) return console.log(err);    
         let user = result.value;
         res.send(user);
     });
@@ -181,7 +227,7 @@ app.put("/api/users", jsonParser, function(req, res){
     collection.findOneAndUpdate({_id: id}, { $set: {password: userPassword, name: userName, email: userEmail, number:userNumber}},
          {returnOriginal: false },function(err, result){
                
-        if(err) return// console.log(err);     
+        if(err) return console.log(err);     
         const user = result.value;
         res.send(user);
     });
@@ -192,3 +238,41 @@ process.on("SIGINT", () => {
     dbClient.close();
     process.exit();
 }); 
+
+app.post("/index-tovar", urlencodedParser, function (request, response) {
+	const userPhoto = request.body.photo;
+	const userText = request.body.text;
+	const userPrice = request.body.price;
+	console.log( request.body.photo);
+
+	  const user = {photo:userPhoto, text:userText, price:userPrice};
+	  const db = mongoClient.db("tovarsDB");
+	  const collection = db.collection("tovars");
+	  collection.find({}).toArray(function(err, tovars){
+		  console.log(tovars);
+	});
+	  collection.insertOne(user, function(err, result){
+		  if(err) return console.log(err);
+		  response.render(__dirname+"/src/index-tovar.hbs", {
+		  });
+	  });
+  });
+
+  app.get("/tovars/:tovarId", function(request, response){
+    const db = mongoClient.db("tovarsDB");
+    const collection = db.collection("tovars");
+    collection.findOne({photo: request.params["tovarId"]}, function(err, result){
+               if(err) return console.log(err);
+               if(result != null)
+                       response.render(__dirname+"/src/tov.hbs", {
+                        photo: result.photo,
+                        text: result.text,
+                        price: result.price
+                       })
+                       else
+                       {
+                           response.render(__dirname+"/src/tov.hbs", {
+                           })
+                       }
+           });
+});
